c44b9b9d04b85fc1ac04f6361495fe6d36917fb7
feikaixin <954630886@qq.com>
2018-10-10 13:48:32 +0800

    init

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+
+export default () =>
+  <div>
+    <Head>
+      <title>My page title</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <p>Hello world!</p>
+  </div>
