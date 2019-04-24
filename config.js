36531f52aa647fc0b92dbb804989876e04c880ce
feikaixin <feikaixin@baidu.com>
2019-04-24 13:06:23 +0800

    配置修改

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port : 3000,
-  domain: 'http://106.14.209.107',
+  // domain: 'http://106.14.209.107',
   //domain: 'http://172.19.42.174',
-  //domain: 'http://localhost'
+  domain: 'http://localhost'
 }
